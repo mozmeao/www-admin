cd49868fdac26e8ad9d9d8726826426442ea92b6
Paul McLanahan <pmac@mozilla.com>
2019-01-17 12:33:08 -0500

    Process card data in new folder layout

--- process_data.py
+++ process_data.py
@@ -3,7 +3,6 @@
 import hashlib
 import json
 import sys
-from itertools import chain
 from pathlib import Path
 from shutil import rmtree, copy
 
@@ -13,7 +12,6 @@ from ruamel.yaml import YAML
 
 yaml = YAML()
 OUTPUT_PATH = Path('output')
-IMAGES_PATH = Path('static/img')
 IMAGES_HASHED = OUTPUT_PATH / 'static/img'
 CARDS_PATH = Path('content')
 CARDS_POST_PATH = OUTPUT_PATH / 'content'
@@ -65,17 +63,24 @@ def clean_dirs():
     rmtree(str(OUTPUT_PATH), ignore_errors=True)
 
 
-def get_all_images():
-    images = IMAGES_PATH.glob('**/*.jpg')
-    images = chain(images, IMAGES_PATH.glob('**/*.png'))
-    return images
+def get_file_hash(file_obj):
+    hasher = hashlib.md5()
+    with file_obj.open('rb') as fh:
+        while True:
+            chunk = fh.read(CHUNK_SIZE)
+            if not chunk:
+                break
+
+            hasher.update(chunk)
+
+    return hasher.hexdigest()
 
 
-def get_hashed_filename(file_obj, md5hash):
-    shorthash = md5hash[:12]
+def get_hashed_filename(file_obj):
+    shorthash = get_file_hash(file_obj)[:12]
     name = file_obj.stem
     ext = file_obj.suffix
-    file_obj = IMAGES_HASHED / file_obj.relative_to(IMAGES_PATH)
+    file_obj = IMAGES_HASHED / file_obj.relative_to(CARDS_PATH)
     return file_obj.with_name('%s.%s%s' % (name, shorthash, ext))
 
 
@@ -85,54 +90,52 @@ def get_highres_filename(file_obj):
     return file_obj.with_name('%s-high-res%s' % (name, ext))
 
 
-def hash_filenames(files):
-    files_changed = {}
-    for fobj in files:
-        hasher = hashlib.md5()
-        with fobj.open('rb') as fh:
-            while True:
-                chunk = fh.read(CHUNK_SIZE)
-                if not chunk:
-                    break
-
-                hasher.update(chunk)
-
-        md5hash = hasher.hexdigest()
-        hashed_fobj = get_hashed_filename(fobj, md5hash)
-        hashed_fobj.parent.mkdir(parents=True, exist_ok=True)
-        print('Copying %s to %s' % (fobj, hashed_fobj))
-        copy(fobj, hashed_fobj)
-        files_changed[fobj] = hashed_fobj
-
-    return files_changed
+def process_image_file(fobj):
+    hashed_fobj = get_hashed_filename(fobj)
+    hashed_fobj.parent.mkdir(parents=True, exist_ok=True)
+    print('Copying %s to %s' % (fobj, hashed_fobj))
+    copy(fobj, hashed_fobj)
+    return hashed_fobj
 
 
-def process_data_files(files_changed):
+def process_card_images(card, data):
+    if 'image' in data:
+        image_path = card.parent / data['image']
+        try:
+            hashed_img = process_image_file(image_path)
+        except IOError:
+            raise RuntimeError('Image referenced but not found: %s' % image_path)
+
+        data['image'] = str(hashed_img.relative_to(IMAGES_HASHED))
+        if data.get('include_highres_image', False):
+            highres_path = get_highres_filename(image_path)
+            try:
+                hashed_highres_img = process_image_file(highres_path)
+            except IOError:
+                raise RuntimeError('Image referenced but not found: %s' % highres_path)
+
+            data['highres_image'] = str(hashed_highres_img.relative_to(IMAGES_HASHED))
+            del data['include_highres_image']
+
+
+def get_json_card(card_obj):
+    base_path = card_obj.parents[1]
+    json_card = card_obj.with_suffix('.json')
+    filename = '.'.join(json_card.parts[2:])
+    return OUTPUT_PATH / base_path / filename
+
+
+def process_card_files():
     """Ingest .md files and produce .json files
 
     Replace image paths with the hashed filename equivalents.
     """
     cards = CARDS_PATH.glob('**/*.md')
     for card in cards:
-        lines = []
         print('Processing %s' % card)
         data = parse_md_file(card)
-        if 'image' in data:
-            image_path = IMAGES_PATH / data['image']
-            if image_path in files_changed:
-                data['image'] = str(files_changed[image_path].relative_to(IMAGES_HASHED))
-                if data.get('include_highres_image', False):
-                    highres_path = get_highres_filename(image_path)
-                    if highres_path in files_changed:
-                        data['highres_image'] = str(files_changed[highres_path].relative_to(IMAGES_HASHED))
-                        del data['include_highres_image']
-                    else:
-                        raise RuntimeError('Image referenced but not found: %s' % highres_path)
-            else:
-                raise RuntimeError('Image referenced but not found: %s' % image_path)
-
-        json_card = card.with_suffix('.json')
-        json_card = OUTPUT_PATH / json_card
+        process_card_images(card, data)
+        json_card = get_json_card(card)
         json_card.parent.mkdir(parents=True, exist_ok=True)
         with json_card.open('w', encoding='utf-8') as fh:
             json.dump(data, fh, ensure_ascii=False, indent=2, sort_keys=True)
@@ -140,10 +143,8 @@ def process_data_files(files_changed):
 
 if __name__ == '__main__':
     clean_dirs()
-    images = get_all_images()
-    files_changed = hash_filenames(images)
     try:
-        process_data_files(files_changed)
+        process_card_files()
     except RuntimeError as e:
         sys.exit(str(e))
 
