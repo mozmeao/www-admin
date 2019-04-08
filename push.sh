d124dd9b54ce83c519150f445a038c464a38fb3f
Paul McLanahan <pmac@mozilla.com>
2019-04-08 15:24:52 -0400

    Change branch deployment process
    Fix #17

--- push.sh
+++ push.sh
@@ -23,13 +23,13 @@ export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 case $GIT_BRANCH in
-    stage)
+    master)
         BUCKET=stage
         ;;
     prod)
         BUCKET=prod
         ;;
-    *)
+    dev)
         BUCKET=dev
         ;;
 esac
